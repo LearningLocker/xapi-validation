4ce91f1285b5554b10cdc614cc171768ba07e46b
Ryan Smith <0ryansmith1994@gmail.com>
2017-01-21 11:42:14 +0000

    Improves test coverage for definitions.

--- dist/tests/schemaRules/definition.js
+++ dist/tests/schemaRules/definition.js
@@ -2,9 +2,11 @@
 var describeInteractionProp_1 = require("../describeInteractionProp");
 var describeOptionalProp_1 = require("../describeOptionalProp");
 var factory_1 = require("../factory");
+var itsInvalid_1 = require("../itsInvalid");
 var validData = {};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = function (test) {
+    itsInvalid_1.default(10, 'not an object', test);
     describeOptionalProp_1.default('name', factory_1.languageMap, validData, test);
     describeOptionalProp_1.default('description', factory_1.languageMap, validData, test);
     describeOptionalProp_1.default('type', factory_1.iri, validData, test);
