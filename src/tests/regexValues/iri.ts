ab4fd92de38ec5755a84857555f22c7db6249bc9
Ryan Smith <0ryansmith1994@gmail.com>
2017-01-22 20:31:05 +0000

    Improves tests for IRIs and Timestamps.

--- src/tests/regexValues/iri.ts
+++ src/tests/regexValues/iri.ts
@@ -2,10 +2,21 @@ import Test from '../helpers/test';
 import itsInvalid from '../itsInvalid';
 import itsValid from '../itsValid';
 
-const validData = 'http://www.example.com';
-
 export default (test: Test) => {
   itsInvalid('http', 'not an IRI', test);
   itsInvalid(10, 'not a String', test);
-  itsValid(validData, test);
+  itsValid('http://www.example.com', test);
+
+  // https://mathiasbynens.be/demo/url-regex
+  itsValid('http://fo-o.com/blah_blah_(wikipedia)#cite-1', test);
+  itsValid('https://www.example.com/foo/?bar=b%20az&inga=42&quux', test);
+  itsValid('http://userid:password@example.com:8080/', test);
+  itsValid('http://userid:password@example.com/', test);
+  itsValid('http://142.42.1.1:8080/', test);
+  itsValid('http://foo.com/unicode_(✪)_in_parens', test);
+  itsValid('http://foo.com/(something)?after=parens', test);
+  itsValid('http://code.google.com/events/#&product=browser', test);
+  itsValid('ftp://foo.bar/baz', test);
+  itsValid('http://⌘➡例子.测试✪उदाहरण.परीकإختبار/䨹', test);
+  itsValid('http://-.~_!$&\'()*+,;=:%40:80%2f::::::@example.com', test);
 };
--- src/tests/regexValues/timestamp.ts
+++ src/tests/regexValues/timestamp.ts
@@ -2,11 +2,19 @@ import Test from '../helpers/test';
 import itsInvalid from '../itsInvalid';
 import itsValid from '../itsValid';
 
-const validData = '20090131T230000-0100';
-
 export default (test: Test) => {
-  itsInvalid('2009-01-31T230000-01:00', 'not a Timestamp', test);
   itsInvalid(10, 'not a String', test);
-  itsValid(validData, test);
-  itsValid('2009-01-31T23:00:00-01:00', test); // Extended
+
+  itsValid('20090131T230000-0100', test); // Basic format
+  itsValid('2009-01-31T23:00:00-01:00', test); // Extended format
+  itsValid('20170101', test); // Date basic format
+  itsValid('2017-01-01', test); // Date extended format
+
+  itsInvalid('2009-01-31T230000-01:00', 'not a Timestamp', test);
+  itsInvalid('2017', 'not a Timestamp', test);
+  itsInvalid('2017-01', 'not a Timestamp', test);
+
+  // http://stackoverflow.com/questions/12756159
+  itsInvalid('2013-99-99T04:13:00+00:00', 'not a Timestamp', test);
+  itsValid('0785-10-10T04:13:00+00:00', test);
 };
