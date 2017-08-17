3e71e5e8679a12efbbf322eb60123d387a73847d
James Mullaney <james@ht2.co.uk>
2017-08-17 16:19:57 +0000

    iso8601 fix

--- dist/regexValues/duration.js
+++ dist/regexValues/duration.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var rulr_1 = require("rulr");
 var TypeWarning_1 = require("../warnings/TypeWarning");
-exports.default = rulr_1.checkRegex(/^P((\d+([\.,]\d+)?Y)?(\d+([\.,]\d+)?M)?(\d+([\.,]\d+)?W)?(\d+([\.,]\d+)?D)?)?(T(\d+([\.,]\d+)?H)?(\d+([\.,]\d+)?M)?(\d+([\.,]\d+)?S)?)?$/i, TypeWarning_1.createTypeWarning('ISO Duration'));
+exports.default = rulr_1.checkRegex(/^(-?)P(?=\d|T\d)(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)([DW]))?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?$/, TypeWarning_1.createTypeWarning('ISO Duration'));
