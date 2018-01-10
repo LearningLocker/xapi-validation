2912596770c510733101260cbae863c998f9d799
Ryan Smith <0ryansmith1994@gmail.com>
2018-01-10 15:30:45 +0000

    fix(iri): Fixes iri regex.

--- src/regexValues/iri.ts
+++ src/regexValues/iri.ts
@@ -1,7 +1,7 @@
 import { checkRegex, Rule } from 'rulr';
 import { createTypeWarning } from '../warnings/TypeWarning';
 
-const regex = /^\w+:\/\/[^\/\#\?]+\/?[^\#\?]*\??[^\#]*\#?.*$/i;
+const regex = /^\w+:\/\/[^\/\#\?]*\/?[^\#\?]*\??[^\#]*\#?[^$]*$/i;
 const warningConstructor = createTypeWarning('Internationalized Resource Identifier (IRI)');
 
 export default checkRegex(regex, warningConstructor) as Rule;
